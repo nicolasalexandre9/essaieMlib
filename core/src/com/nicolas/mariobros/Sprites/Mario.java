ad62b30832c62edad9715d86d53578caa37e7925
nicolas <nicolasalexandre9@gmail.com>
2017-01-28 11:11:42 +0100

    video 11 - 3.06

--- core/src/com/nicolas/mariobros/Sprites/Mario.java
+++ core/src/com/nicolas/mariobros/Sprites/Mario.java
@@ -3,26 +3,46 @@ package com.nicolas.mariobros.Sprites;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g3d.model.Animation;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
 import com.nicolas.mariobros.MarioBros;
 import com.nicolas.mariobros.Screens.PlayScreen;
 
 
 public class Mario extends Sprite {
 
+    public enum State {FALLING, JUMPING,STANDING, RUNNNING};
+    public State currentState;
+    public State previousState;
     public World world;
     public Body b2body;
     private TextureRegion marioStand;
+    private Animation marioRun;
+    private Animation marioJump;
+    private float stateTimer;
+    private boolean runningRight;
+
+
 
     public Mario(World world, PlayScreen screen){
         super(screen.getAtlas().findRegion("little_mario"));
         this.world = world;
-        defineMario();
+        currentState = State.STANDING;
+        previousState = State.STANDING;
+        stateTimer = 0;
+        runningRight = true;
+
+        Array<TextureRegion> frames = new Array<TextureRegion>();
+
         marioStand = new TextureRegion(getTexture(), 1, 11, 16, 16);
+
+        defineMario();
         setBounds(0, 10, 16 / MarioBros.PPM, 16 / MarioBros.PPM);
         setRegion(marioStand);
 
